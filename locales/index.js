4f5cce1c818b5bd764608a6c8b87b5084039a063
Patcharapon Wangtiyong <patcharapon@thinknet.co.th>
2017-08-15 09:10:52 +0700

    3. เรียกข้อมูลมาแสดง

--- locales/index.js
+++ locales/index.js
@@ -1,7 +1,7 @@
 import { I18n } from 'react-i18nify'
-import { th } from "./th.json"
-import { en } from "./en.json"
-import { jp } from "./jp.json"
+import th from "./th.json"
+import en from "./en.json"
+import jp from "./jp.json"
 
 I18n.setTranslations({
     th,
@@ -10,7 +10,7 @@ I18n.setTranslations({
 });
 
 // เราจะกำหนดภาษาเริ่มต้นของเราเป็นภาษาไทยนะครับ
-I18n.setLocale('th');
+I18n.setLocale('th')
 
 // เราจะต้อง export library ของ I18n เพิ่อไปใช้ใน File อื่น
 export const locale = I18n
---
+++ pages/hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { locale } from '../locales/index'
+
+export default class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{locale.t('hello')}</h1>
+                <h2>{locale.t('greeting')}</h2>
+            </div>
+        )
+    }
+}
