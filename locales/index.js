34a12aea09c43dbdb25e6770e38dc585925b92b4
Patcharapon Wangtiyong <patcharapon@thinknet.co.th>
2017-08-15 09:24:16 +0700

    กำหนดเพื่อเลือกเปลี่ยนเปนภาษาอื่น

--- locales/index.js
+++ locales/index.js
@@ -12,5 +12,11 @@ I18n.setTranslations({
 // เราจะกำหนดภาษาเริ่มต้นของเราเป็นภาษาไทยนะครับ
 I18n.setLocale('th')
 
+// กำหนดภาษาเป็นภาษาอังกฤษ
+// I18n.setLocale('en')
+
+// กำหนดภาษาเป็นภาษาญี่ปุ่น
+// I18n.setLocale('jp')
+
 // เราจะต้อง export library ของ I18n เพิ่อไปใช้ใน File อื่น
 export const locale = I18n
