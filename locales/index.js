0af415542e0b1e1d124569084c08dad76487767f
Patcharapon Wangtiyong <patcharapon@thinknet.co.th>
2017-08-15 08:40:06 +0700

    2. import ข้อมูลไปยัง i18nify

---
+++ locales/index.js
@@ -0,0 +1,16 @@
+import { I18n } from 'react-i18nify'
+import { th } from "./th.json"
+import { en } from "./en.json"
+import { jp } from "./jp.json"
+
+I18n.setTranslations({
+    th,
+    en,
+    th,
+});
+
+// เราจะกำหนดภาษาเริ่มต้นของเราเป็นภาษาไทยนะครับ
+I18n.setLocale('th');
+
+// เราจะต้อง export library ของ I18n เพิ่อไปใช้ใน File อื่น
+export const locale = I18n
